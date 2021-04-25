fe3d53ae0580568aab898758b8778e4aaf6d4305
tauhidcp <ahmad.tauhid.cp@gmail.com>
2021-04-25 11:24:12 +0800

    nambahin update baru

---
+++ example/d_data_bank.php
@@ -0,0 +1,21 @@
+<?php 
+require_once("connect.php");
+$sql = "select * from modul_bank";
+$qry = mysqli_query($con,$sql);
+echo "<table border='1'>";
+echo "<tr style='background-color:#99DDCC'>";
+echo "<td>ID</td> <td>NAME_BANK</td> <td>NO_REKENING</td> <td>AN_REKENING</td> <td>INTEGRASI_COA</td> <td>SALDO_BANK</td>";
+echo "</tr>";
+while ($data = mysqli_fetch_assoc($qry)){
+echo "<tr>";
+echo "<td>".$data['ID']."</td>";
+echo "<td>".$data['NAME_BANK']."</td>";
+echo "<td>".$data['NO_REKENING']."</td>";
+echo "<td>".$data['AN_REKENING']."</td>";
+echo "<td>".$data['INTEGRASI_COA']."</td>";
+echo "<td>".$data['SALDO_BANK']."</td>";
+echo "</tr>";
+}
+
+echo "</table>";
+?>
--- index.php
+++ index.php
@@ -1,7 +1,38 @@
-<h4>Example List Menu</h4>
-<ul>
-	<li><a href="example/jurnal_transaksi.php" target="_blank">Laporan Jurnal Transaksi</a></li>
-	<li><a href="example/buku_besar.php" target="_blank">Laporan Buku Besar</a></li>
-	<li><a href="example/profit_loss.php" target="_blank">Laporan Laba &amp; Rugi (Profit &amp; Loss)</a></li>
-	<li><a href="example/balancing.php" target="_blank">Laporan Balancing Sheet (Neraca)</a></li>
-</ul>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Laporan Akuntansi Sederhana</title>
+	<style type="text/css">
+		ul li{
+		  margin: 5px 0;
+		}
+	</style>
+</head>
+
+<body>
+	<h4>Menu</h4>
+	<ul>
+		<li> <b>Data Master</b></br>
+			<ul>
+				<li><a href="example/d_data_akun.php" target="_blank">Data Akun</a></li>
+				<li><a href="example/d_data_bank.php" target="_blank">Data Bank</a></li>
+			</ul>
+		</li>
+		<li> <b>Transaksi</b></br>
+			<ul>
+				<li><a href="example/t_jurnal_transaksi.php" target="_blank">Jurnal Transaksi</a></li>
+				<li><a href="example/t_buku_besar.php" target="_blank">Transaksi Bank</a></li>
+				<li><a href="example/t_profit_loss.php" target="_blank">Arsip Jurnal</a></li>
+			</ul>
+		</li>
+		<li> <b>Laporan</b></br>
+			<ul>
+				<li><a href="example/l_jurnal_transaksi.php" target="_blank">Laporan Jurnal Transaksi</a></li>
+				<li><a href="example/l_buku_besar.php" target="_blank">Laporan Buku Besar</a></li>
+				<li><a href="example/l_profit_loss.php" target="_blank">Laporan Laba &amp; Rugi (Profit &amp; Loss)</a></li>
+				<li><a href="example/l_balancing.php" target="_blank">Laporan Balancing Sheet (Neraca)</a></li>
+			</ul>
+		</li>
+	</ul>
+</body>
+</html>
